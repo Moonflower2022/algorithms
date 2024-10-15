d437244223bdbafa3cc8d59681d8ebec7d18c852
Moonflower2022 <harqian@nuevaschool.org>
2024-10-15 14:41:59 -0700

    removed a debugging print statement and added a utility function to generate preference lists

--- stable_matching_problem/implementation.py
+++ stable_matching_problem/implementation.py
@@ -34,7 +34,6 @@ class StableMatcher:
                     if a != self.most_preferable(front_door, i):
                         self.a_statuses[a] += 1
                         front_door.remove(a)
-                        print(self.a_statuses[a])
                         self.b_front_doors[self.a_preferences_list[a][self.a_statuses[a]]].append(a)
 
     def match(self):
@@ -46,3 +45,14 @@ class StableMatcher:
             [front_door[0] for front_door in self.b_front_doors],
             [i for i in range(len(self.b_front_doors))],
         ))
+    
+def get_preferences_list(a_list, b_list, evaluation_function):
+    preferences_list = []
+
+    for a in a_list:
+        evaluations = [(evaluation_function(a, b), i) for i, b in enumerate(b_list)]
+        sorted_evaluations = sorted(evaluations, key=lambda pair: pair[0])
+        sorted_indicies = [element[1] for element in sorted_evaluations]
+
+        preferences_list.append(sorted_indicies)
+    return preferences_list
